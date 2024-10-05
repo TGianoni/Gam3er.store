dcc630a9e0625d202bdabaa52f874439e0886e2c
Thiago Gianoni <gianoni2@hotmail.com>
2024-10-05 18:40:20 -0300

    frontend

---
+++ src/app/produto/[id]/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import useProdutos from '@/data/hooks/useProdutos'
+
+export default function PaginaProduto(props: any) {
+    const { produtos } = useProdutos()
+
+    const id = +props.params.id
+    const produto = produtos.find((p) => p.id === id)
+    return produto ? <div>{produto?.nome}</div> : <div>Produto não encontrado</div>
+}
